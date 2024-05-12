85bb6a02385b931d379ecc64a14c019f567634f9
yihenew <yihenewu89@gmail.com>
2024-05-12 19:57:53 +0300

    the title changed

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page and this my'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page and this myFlutter Demo Home Page and this myFlutter Demo Home Page and this my'),
     );
   }
 }
